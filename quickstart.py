086824665bdf6a6fd2c2aa1e35f5d1896a2f6ad7
Bui Hoang Hai <beo@Buis-MacBook-Pro.local>
2020-01-02 20:40:56 +0700

    Version 2.0 more friendly to users

--- quickstart.py
+++ quickstart.py
@@ -1,32 +1,90 @@
-import string
+# import string
+# import pandas as pd
+
+# f = open('TKBKMAVN-guest.html')
+# content = f.read()
+# f.close()
+
+# df = pd.read_html(content)
+# dfsubpart1 = df[1]
+# dfsubpart2 = df[2]
+# dfschedule = df[3]
+
+# writer = pd.ExcelWriter('outp.xlsx', engine='xlsxwriter')
+# dfsubpart1.to_excel(writer, sheet_name='Sheet1', startcol=3)
+# dfsubpart2.to_excel(writer, sheet_name='Sheet1', startcol=8)
+# dfschedule.to_excel(writer, sheet_name='Sheet1', startrow=8)
+
+# book = writer.book
+# sheet = writer.sheets['Sheet1']
+# wrap_format = book.add_format({'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
+
+# d = dict(zip(range(26), list(string.ascii_uppercase)))
+# for col in range(dfschedule.shape[1] + 1):
+#     excel_header = d[col] + ':' + d[col]
+#     sheet.set_column(excel_header, None, wrap_format)
+
+# for col in range(dfschedule.shape[1] + 1):
+#     if col > 2:
+#         excel_header = d[col] + ':' + d[col]
+#         sheet.set_column(excel_header, 25, wrap_format)
+
+# writer.save()
+
+
+import os
 import pandas as pd
+import string
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
 
-f = open('TKBKMAVN-guest.html')
-content = f.read()
-f.close()
+retrieve_url = 'http://tkbkmavnteam.tk'
+path = os.path.dirname(os.path.realpath(__file__))
+download_path = path + '/image'
+options = Options()
+options.add_experimental_option('prefs', {
+    'download.default_directory': download_path,
+    'download.prompt_for_download': False,
+    'safebrowsing.enabled': False,
+    'download.directory_upgrade': False,
+    'safebrowsing_for_trusted_sources_enabled': False
+})
 
-df = pd.read_html(content)
-dfsubpart1 = df[1]
-dfsubpart2 = df[2]
-dfschedule = df[3]
+before = os.listdir(download_path)
 
-writer = pd.ExcelWriter('outp.xlsx', engine='xlsxwriter')
-dfsubpart1.to_excel(writer, sheet_name='Sheet1', startcol=3)
-dfsubpart2.to_excel(writer, sheet_name='Sheet1', startcol=8)
-dfschedule.to_excel(writer, sheet_name='Sheet1', startrow=8)
+# username = input('Enter your ID: ')
+# password = input('Enter your password: ')
+username = 'CT020115'
+password = 'falcon99'
 
-book = writer.book
-sheet = writer.sheets['Sheet1']
-wrap_format = book.add_format({'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
+driver = webdriver.Chrome(executable_path='{}/chromedriver'.format(path), options=options)
+driver.get(retrieve_url)
 
-d = dict(zip(range(26), list(string.ascii_uppercase)))
-for col in range(dfschedule.shape[1] + 1):
-    excel_header = d[col] + ':' + d[col]
-    sheet.set_column(excel_header, None, wrap_format)
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-login/div/div/div[2]/div[4]').click()
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/p/input').click()
+driver.find_element_by_xpath('//*[@id="RoomBoxChat"]/div[1]/div[1]/div[3]').click()
+driver.find_element_by_tag_name('html').send_keys(Keys.END)
 
-for col in range(dfschedule.shape[1] + 1):
-    if col > 2:
-        excel_header = d[col] + ':' + d[col]
-        sheet.set_column(excel_header, 25, wrap_format)
+time.sleep(10)
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/div').click()
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[4]/a/img').click()
 
-writer.save()
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[2]/div/input').send_keys(username)
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[3]/div/input').send_keys(password)
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[4]/button[2]').click()
+
+time.sleep(5)
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[1]/a/img').click()
+driver.find_element_by_xpath('/html/body/div[2]/app-root/div[4]/div/div[2]/div[2]/div/img').click()
+time.sleep(5)
+driver.close()
+
+print(download_path)
+after = os.listdir(download_path)
+change = set(after) - set(before)
+filename = None
+if len(change) == 1:
+    filename = change.pop()
+print('--- Saved into image named {}'.format(filename))
--- retrievedSchedule.py
+++ retrievedSchedule.py
@@ -9,110 +9,188 @@ import hashlib
 
 retrieve_url = 'http://tkbkmavnteam.tk'
 path = os.path.dirname(os.path.realpath(__file__))
-download_path = path + '/downloads'
 
-before = os.listdir(download_path)
+def setupDriverForDownloadMethod(download_path):
+    print(download_path)
+    options = Options()
+    options.add_experimental_option('prefs', {
+        'download.default_directory': download_path,
+        'download.prompt_for_download': False,
+        'safebrowsing.enabled': False,
+        'download.directory_upgrade': True,
+        'safebrowsing_for_trusted_sources_enabled': False
+    })
+    return options
 
-options = Options()
-options.add_experimental_option('prefs', {
-    'download.default_directory': download_path,
-    'download.prompt_for_download': False,
-    'safebrowsing.enabled': False,
-    'download.directory_upgrade': True,
-    'safebrowsing_for_trusted_sources_enabled': False
-})
+def createDriverWithOptions(options):
+    driver = webdriver.Chrome(executable_path='{}/chromedriver'.format(path), options=options)
+    print('--- Getting the webdriver to scrapping...')
+    return driver
 
-# username = 'CT020115'
-# f = open('password.txt', 'r')
-# password = f.readline()
-# f.close()
+def createDriver():
+    driver = webdriver.Chrome(executable_path='{}/chromedriver'.format(path))
+    print('--- Getting the webdriver to scrapping...')
+    return driver
 
-username = str(input('Enter your studentID: '))
-password = str(input('Enter your password: '))
+def getUserLoginData():
+    username = str(input('Enter your studentID: '))
+    password = str(input('Enter your password: '))
+    return username, password
 
-driver = webdriver.Chrome(executable_path='{}/chromedriver'.format(path), options=options)
-driver.get(retrieve_url)
+def getLoginAsGuest(driver):
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-login/div/div/div[2]/div[4]').click()
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/p/input').click()
+    driver.find_element_by_xpath('//*[@id="RoomBoxChat"]/div[1]/div[1]/div[3]').click()
+    driver.find_element_by_tag_name('html').send_keys(Keys.END)
+    print('--- Logging in as a guest...')
 
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-login/div/div/div[2]/div[4]').click()
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/p/input').click()
-driver.find_element_by_xpath('//*[@id="RoomBoxChat"]/div[1]/div[1]/div[3]').click()
-driver.find_element_by_tag_name('html').send_keys(Keys.END)
+def Await(seconds):
+    time.sleep(seconds)
 
-time.sleep(10)
+def getLoginUser(driver, username, password):
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/div').click()
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[4]/a/img').click()
 
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-app-privacy-policy/div/div/div/div').click()
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[4]/a/img').click()
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[2]/div/input').send_keys(username)
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[3]/div/input').send_keys(password)
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[4]/button[2]').click()
+    print('--- Getting schedule from https://actvn.edu.vn')
 
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[2]/div/input').send_keys(username)
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[3]/div/input').send_keys(password)
-driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-setting-app/div/div/div/div[2]/form/div[4]/button[2]').click()
+def createExcelWriter():
+    writer = pd.ExcelWriter('outp.xlsx', engine='xlsxwriter')
+    return writer
 
-time.sleep(3)
-# headers = driver.find_elements_by_xpath('/html/body/div[2]/app-root/div[3]/app-mainapp/div/div/div[2]/table/tr/td[1]/table/thead/tr/th')
-# subjects = driver.find_elements_by_xpath('/html/body/div[2]/app-root/div[3]/app-mainapp/div/div/div[2]/table/tr/td[1]/table/tbody/tr/td')
-# print(subjects[1].text, subjects[2].text)
+def setDataFrameDownloaded(df, writer):
+    dfs = df[1]
+    dfs.to_excel(writer, sheet_name='Sheet1', startcol=3)
+    dfs = df[2]
+    dfs.to_excel(writer, sheet_name='Sheet1', startcol=8)
+    dfs = df[3]
+    dfs.to_excel(writer, sheet_name='Sheet1', startrow=8)
+    print('--- Got data from original webURL')
+    return dfs
 
-# sub_table = []
-# for _ in headers:
-#     sub_table.append((_.text, []))
+def setDataFrame(driver, writer):
+    subjects = driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-mainapp/div/div/div[2]/table/tr')
+    sublist1 = subjects.find_element_by_xpath('./td[{}]'.format(1)).get_attribute('innerHTML')
+    df = pd.read_html(sublist1)
+    dfs = df[0]
+    dfs.to_excel(writer, sheet_name='Sheet1', startcol=3)
 
-# for i in range(len(subjects)):
-#     if i % 3 == 0:
-#         sub_table[0][1].append(subjects[i].text)
-#     elif i % 3 == 1:
-#         sub_table[1][1].append(subjects[i].text)
-#     else:
-#         sub_table[2][1].append(subjects[i].text)
+    sublist2 = subjects.find_element_by_xpath('./td[{}]'.format(2)).get_attribute('innerHTML')
+    df = pd.read_html(sublist2)
+    dfs = df[0]
+    dfs.to_excel(writer, sheet_name='Sheet1', startcol=8)
 
-# driver.quit()
+    subjects = driver.find_element_by_xpath('/html/body/div[2]/app-root/div[3]/app-mainapp/div/div/div[2]/div').get_attribute('innerHTML')
+    df = pd.read_html(subjects)
+    dfs = df[0]
+    dfs.to_excel(writer, sheet_name='Sheet1', startrow=8)
+    print('--- Got data from original webURL')
+    return dfs
 
-# Dict = {title: column for (title, column) in sub_table}
-# print(Dict)
-# df = pd.DataFrame(Dict)
-# df.to_excel('schedule.xlsx', sheet_name='Sheet_name_1')
+def settingExcelFile(writer, df):
+    book = writer.book
+    sheet = writer.sheets['Sheet1']
+    wrap_format = book.add_format({'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
 
-download_btn = driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[1]/a/img')
-download_btn.click()
-time.sleep(2)
-file_download_formatter = driver.find_element_by_xpath('/html/body/div[2]/app-root/div[4]/div/div[2]/div[1]/div/img')
-file_download_formatter.click()
+    d = dict(zip(range(26), list(string.ascii_uppercase)))
 
-driver.close()
-
-after = os.listdir(download_path)
-change = set(after) - set(before)
-filename = None
-if len(change) == 1:
-    filename = change.pop()
-
-# convert HTML file to Dictionary
-html_file = download_path + '/' + filename
-f = open(html_file, 'r')
-content = f.read()
-f.close()
-
-df = pd.read_html(content)
-dfsubpart1 = df[1]
-dfsubpart2 = df[2]
-dfschedule = df[3]
-
-writer = pd.ExcelWriter('outp.xlsx', engine='xlsxwriter')
-dfsubpart1.to_excel(writer, sheet_name='Sheet1', startcol=3)
-dfsubpart2.to_excel(writer, sheet_name='Sheet1', startcol=8)
-dfschedule.to_excel(writer, sheet_name='Sheet1', startrow=8)
-
-book = writer.book
-sheet = writer.sheets['Sheet1']
-wrap_format = book.add_format({'text_wrap': True, 'align': 'center', 'valign': 'vcenter'})
-
-d = dict(zip(range(26), list(string.ascii_uppercase)))
-for col in range(dfschedule.shape[1] + 1):
-    excel_header = d[col] + ':' + d[col]
-    sheet.set_column(excel_header, None, wrap_format)
-
-for col in range(dfschedule.shape[1] + 1):
-    if col > 2:
+    for col in range(3, df.shape[1] + 1):
         excel_header = d[col] + ':' + d[col]
         sheet.set_column(excel_header, 25, wrap_format)
+    writer.save()
 
-writer.save()
+def DownloadHTMLMethod():
+    # Get access key
+    username, password = getUserLoginData()
+    download_path = path + '/downloads'
+    before = os.listdir(download_path)
+    # setup download options for Chrome_options
+    options = setupDriverForDownloadMethod(download_path)
+    # Create WebDriver for Chrome
+    driver = createDriverWithOptions(options)
+    # Access the webURL
+    driver.get(retrieve_url)
+    # Get Login as Guest
+    getLoginAsGuest(driver)
+    print('--- Waiting for Response to accept License...')
+    Await(10)
+    # Click to get login to URL
+    getLoginUser(driver, username, password)
+    Await(3)
+    writer = createExcelWriter()
+    print('--- Downloading HTML file...')
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[1]/a/img').click()    # download button clicked
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[4]/div/div[2]/div[1]/div/img').click()         # HTML file download Clicked
+    Await(3)
+    after = os.listdir(download_path)
+    change = set(after) - set(before)
+    filename = None
+    if len(change) == 1:
+        filename = change.pop()
+
+    # ------------- Open HTML file downloaded above -------------
+    html_file = download_path + '/' + filename
+    f = open(html_file, 'r')
+    content = f.read()
+    f.close()
+
+    df = pd.read_html(content)
+    dfs = setDataFrameDownloaded(df, writer)
+    driver.close()
+    settingExcelFile(writer, dfs)
+    print('--- Saved into outp.xlsx file successfully!')
+
+def DownloadImage():
+    username, password = getUserLoginData()
+    download_path = path + '/image'
+    before = os.listdir(download_path)
+    options = setupDriverForDownloadMethod(download_path)
+    driver = createDriverWithOptions(options)
+    driver.get(retrieve_url)
+    getLoginAsGuest(driver)
+    print('--- Waiting for Response to accept License...')
+    Await(10)
+    getLoginUser(driver, username, password)
+    Await(3)
+    print('--- Downloading Image...')
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[2]/ul/li[1]/a/img').click()
+    driver.find_element_by_xpath('/html/body/div[2]/app-root/div[4]/div/div[2]/div[2]/div/img').click()
+    Await(3)
+    after = os.listdir(download_path)
+    change = set(after) - set(before)
+    filename = None
+    if len(change) == 1:
+        filename = change.pop()
+    print('--- Saved into image named {}'.format(filename))
+
+def WebScrappingMethod():
+    username, password = getUserLoginData()
+    driver = createDriver()
+    driver.get(retrieve_url)
+    getLoginAsGuest(driver)
+    print('--- Waiting for Response to accept License...')
+    Await(10)
+    getLoginUser(driver, username, password)
+    Await(3)
+    writer = createExcelWriter()
+    dfs = setDataFrame(driver, writer)
+    driver.close()
+    settingExcelFile(writer, dfs)
+    print('--- Saved into outp.xlsx file successfully!')
+
+def main():
+    print('-- 1. Download HTML file --')
+    print('-- 2. Download Image --')
+    print('-- 3. Web scrapping --')
+    choice = int(input('Choose a method from above: '))
+    if choice == 1:
+        DownloadHTMLMethod()
+    elif choice == 2:
+        DownloadImage()
+    else:
+        WebScrappingMethod()
+
+if __name__ == '__main__':
+    main()
